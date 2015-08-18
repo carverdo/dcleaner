ccdf1971a8dd134eefeb6d69c830e373cd44a0de
carverdo <donal.carville@gmail.com>
2015-08-18 12:51:49 +0100

    Tweaked DevelConfig

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ class DevelConfig(Config):
     """
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DEV_DATABASE_URL', Config.DEVEL_DATABASE_NAME)
+        'DATABASE_URL', Config.DEVEL_DATABASE_NAME)  ##
 
 
 class ProdConfig(Config):
