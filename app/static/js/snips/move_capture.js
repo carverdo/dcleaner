96b0df019c3688fb0a3d1f55854418f3f2f59ba7
carverdo <donal.carville@gmail.com>
2015-11-10 17:01:20 +0000

    Fixing compass

--- app/static/js/snips/move_capture.js
+++ app/static/js/snips/move_capture.js
@@ -264,32 +264,33 @@ function flashText(cell_ref) {
 
 
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-var degtorad = Math.PI / 180; // Degree-to-Radian conversion
-
-function compassHeading( alpha, beta, gamma ) {
-  var _x = beta  ? beta  * degtorad : 0; // beta value
-  var _y = gamma ? gamma * degtorad : 0; // gamma value
-  var _z = alpha ? alpha * degtorad : 0; // alpha value
-
-  var cX = Math.cos( _x );
-  var cY = Math.cos( _y );
-  var cZ = Math.cos( _z );
-  var sX = Math.sin( _x );
-  var sY = Math.sin( _y );
-  var sZ = Math.sin( _z );
-
-  // Calculate Vx and Vy components
-  var Vx = - cZ * sY - sZ * sX * cY;
-  var Vy = - sZ * sY + cZ * sX * cY;
-
-  // Calculate compass heading
-  var compassHeading = Math.atan( Vx / Vy );
-
-  // Convert compass heading to use whole unit circle
-  if( Vy < 0 ) {
-    compassHeading += Math.PI;
-  } else if( Vx < 0 ) {
-    compassHeading += 2 * Math.PI;
-  }
-  return compassHeading * ( 180 / Math.PI ); // Compass Heading (in degrees)
+function compassHeading(alpha, beta, gamma) {
+    if (beta != 0 || gamma !=0) {
+        // degrees to radians
+        var alphaRad = alpha * (Math.PI / 180);
+        var betaRad = beta * (Math.PI / 180);
+        var gammaRad = gamma * (Math.PI / 180);
+        // sines and cosines
+        var sA = Math.sin(alphaRad);
+        var sB = Math.sin(betaRad);
+        var sG = Math.sin(gammaRad);
+        var cA = Math.cos(alphaRad);
+        var cB = Math.cos(betaRad);
+        var cG = Math.cos(gammaRad);
+        // Calculate A, B, G rotations
+        var rA = - cA * sG - sA * sB * cG;
+        var rB = - sA * sG + cA * sB * cG;
+        var rG = - cB * cG;
+        // Compass heading
+        var compassHeading = Math.atan(rA / rB);
+        // Convert compass heading to use whole unit circle
+        if(rB < 0) {
+            compassHeading += Math.PI;
+        } else if(rA < 0) {
+            compassHeading += 2 * Math.PI;
+        }
+        return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
+    } else {
+        return 360 - alpha;
+    }
 }
