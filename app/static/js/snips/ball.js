1c839fac7783104b437900568f4f57217ae96477
carverdo <donal.carville@gmail.com>
2015-12-04 17:35:38 +0000

    Add tiltclock

--- app/static/js/snips/ball.js
+++ app/static/js/snips/ball.js
@@ -272,8 +272,8 @@ function passMotionData() {
         'strBeta': JSON.stringify(rot['beta']),
         'strGamma': JSON.stringify(rot['gamma'])
     }
-    $.getJSON('./_balldata', strData, function(data) {
-        $("#result").text("Signature received.").fadeOut(8000); // .text(data.ballData);
+    $.getJSON('./_balldata', strData, function() { // data
+        $("#result").text("Signature received.").fadeIn().fadeOut(8000); // .text(data.ballData);
     });
 };
 
@@ -304,7 +304,7 @@ function handleOrientation(event) {
             prettyButtons('on', remTime);
             acc['x'].push(acc_x);
             acc['y'].push(acc_y);
-            // drawCanvas();
+            drawCanvas();
             rot['theta'].push(northFace);
             rot['beta'].push(dir_b);
             rot['gamma'].push(dir_g);
